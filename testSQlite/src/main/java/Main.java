4311763d4bf90534030c273976c0f2521ba2f09e
gitpavelal <mr.pavel.a@mail.ru>
2016-06-17 09:31:50 +0300

    нет изменений

--- testSQlite/src/main/java/Main.java
+++ testSQlite/src/main/java/Main.java
@@ -2,6 +2,8 @@ import dao.BookDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
+
 
 public class Main {
 
@@ -9,11 +11,14 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+
 //        logger.info("START app!");
-
         Menu menu = new Menu();
-
         menu.viewMenu();
+
+;
+
 
 
     }
