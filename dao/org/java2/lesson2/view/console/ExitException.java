d3e38e296e075b78646aa071b61d746b3be3daa6
gitpavelal <mr.pavel.a@mail.ru>
2016-06-18 17:02:41 +0300

    ok

--- dao/org/java2/lesson2/view/console/ExitException.java
+++
@@ -1,8 +0,0 @@
-package org.java2.lesson2.view.console;
-
-/**
- * Ошибка для завершения приложения
- */
-public class ExitException extends RuntimeException {
-    //no-op
-}
--- dao/org/java2/lesson2/view/console/MainMenuException.java
+++
@@ -1,7 +0,0 @@
-package org.java2.lesson2.view.console;
-
-/**
- * Ошибка для выхода в главное меню
- */
-public class MainMenuException extends RuntimeException {
-}
